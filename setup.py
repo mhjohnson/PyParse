14b9e45ce380625b8df4a4c177ef51a65ad9c17f
Matthew Johnson <johnson.matthew.h@gmail.com>
2013-09-22 22:38:54 -0400

    setup works with root package

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup
-setup(name='EasyParse',
-      author='Matthew H. Johnson, PharmD',
+setup(name='PyParse',
+      author='Matt Johnson, PharmD.',
+      author_email='johnson.matthew.h@gmail.com',
       version='1.0',
+      package_dir = {'PyParse':''},
+      packages = ['PyParse'],
       url = 'https://github.com/mhjohnson/PyParse',
-      py_modules=['parser']
 )
