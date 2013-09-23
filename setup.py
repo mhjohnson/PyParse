0c1c463efbdde18d51fe3c835ebaffd42a45a19b
Matthew Johnson <johnson.matthew.h@gmail.com>
2013-09-22 22:51:20 -0400

    only including parser module

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ setup(name='PyParse',
       author='Matt Johnson, PharmD.',
       author_email='johnson.matthew.h@gmail.com',
       version='1.0',
-      package_dir = {'PyParse':''},
-      packages = ['PyParse'],
+      py_modules=['parser'],
       url = 'https://github.com/mhjohnson/PyParse',
 )
